a9075bfb7f49f9309fea2251cb5c33e4aa6094b1
Zac <hamannzac@gmail.com>
2023-07-26 15:14:12 -0500

    Add inner shadow to card image

--- components/card.tsx
+++ components/card.tsx
@@ -14,11 +14,11 @@ const Card: React.FC<CardProps> = ({ title, description, link, image }) => {
       className="
         mt-8 basis-[45%] relative bg-white
         border-8 border-white rounded-xl
-        shadow-lg shadow-black/25 
+        shadow-lg shadow-black/25
         hover:text-blue-500 hover:shadow-xl hover:shadow-black/30
       "
     >
-      <div className="relative h-48 rounded-lg overflow-hidden">
+      <div className="relative h-48 rounded-lg overflow-hidden shadow-inner">
         <Image
           src={image}
           alt={title}
